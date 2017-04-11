e1098fce78a65c206f3dc4951551c1599c39ab66
Sergey Zhuravlev <sergey4zhuravlev@gmail.com>
2017-04-12 00:44:00 +0300

    Emacs code auto indent

--- etc/clojure_startup.el
+++ etc/clojure_startup.el
@@ -1,108 +1,102 @@
-;usings:
+					;usings:
 (require 'subr-x)
 
 
-
-;clojure environment:
+					;clojure environment:
 (defvar lein-project-directory "~")
+
 (defvar lein-executer 
-   (cond 
-		((equal system-type "windows-nt") "lein.bat " )
-		((equal system-type "gnu/linux") "lein " )
-		(t "lein ")
-	)
-)
-;(defvar lein-project-name "hello-world")
-;(defvar lein-template "default")
+  (cond 
+   ((equal system-type "windows-nt") "lein.bat " )
+   ((equal system-type "gnu/linux") "lein " )
+   (t "lein ")))
+
+(defvar lein-project-file "project.clj")
 
 
+					;clojure tools:
+(defun read-lein-directory ()
+  (setq lein-project-directory (read-directory-name "Lein project directory: "))
+  (message "Lein project directory is %s" lein-project-directory))
 
-;clojure tools:
-(defun update-lein-directory () (setq lein-project-directory (read-directory-name "Lein project directory: ")) (print lein-project-directory))
-(defun update-lein-project-name () (setq lein-project-name (read-string-not-whitespace "Lein project name")))
-(defun update-lein-template () (setq lein-template (read-string-not-whitespace "Lein template name (default - for library; app - for application)" "default" "lein template name")))
+(defun read-lein-project-name ()
+  (setq lein-project-name (read-string-not-whitespace "Lein project name")))
+
+(defun load-lein-project-name ()
+  nil) ;unimplemented
+
+(defun read-lein-template ()
+  (setq lein-template (read-string-not-whitespace "Lein template name (default - for library; app - for application)" "default" "lein template name")))
 
 
-
-;clojure commands:
+					;clojure commands:
 (defun lein-help ()
   (interactive)
   (shell-command (concat lein-executer "help " (read-string "Input Lein params for help or RET: "))))
-  
-(defun lein-execute-command ()
-	(interactive)
-	(let ((default-directory lein-project-directory))
-		(shell-command 
-			(concat lein-executer (read-string "Input Lein params: "))
-		)
-	)
-)
 
-(defun set-lein-project-directory ()
-	(interactive)
-	(let ((default-directory lein-project-directory))
-		(update-lein-directory)
-	)
-)
+(defun lein-execute-command ()
+  (interactive)
+  (let ((default-directory lein-project-directory))
+    (shell-command (concat lein-executer (read-string "Input Lein params: ")))))
+
+(defun lein-set-project-directory ()
+  (interactive)
+  (let ((default-directory lein-project-directory))
+    (read-lein-directory)
+    (load-lein-project-name)))
 
 (defun lein-new-project ()
-	(interactive)
-	(let ((default-directory lein-project-directory))
-		(update-lein-directory)
-		(read-string (concat "Directory '" lein-project-directory "'. Press RET"))
-		(if (file-exists-p lein-project-directory) nil (make-directory lein-project-directory))
-		(update-lein-template)
-		(update-lein-project-name)
-		(let ((default-directory lein-project-directory))
-			;(if 
-				;(eq 0
-					(shell-command 
-						(concat lein-executer "new " lein-template " " lein-project-name )
-					)
-				;)
-				(setq lein-project-directory (path-concat lein-project-directory lein-project-name))
-				;nil
-			;)
-		)
-	)
-)
+  (interactive)
+  (let ((default-directory lein-project-directory)) (update-lein-directory)
+       (read-string (concat "Directory '" lein-project-directory "'. Press RET"))
+       (if (file-exists-p lein-project-directory) nil (make-directory lein-project-directory))
+       (read-lein-template)
+       (read-lein-project-name)
+       (let ((default-directory lein-project-directory))
+	 (shell-command 
+	  (concat lein-executer "new " lein-template " " lein-project-name ))
+	 (setq lein-project-directory (path-concat lein-project-directory lein-project-name)))))
 
 (defun lein-full-compile-project ()
-	(interactive)
-	(let ((default-directory lein-project-directory))
-		(shell-command 
-			(concat lein-executer " clean && " lein-executer " deps && " lein-executer " compile && " lein-executer " uberjar")
-		)
-	)
-)
+  (interactive)
+  (let ((default-directory lein-project-directory))
+    (shell-command 
+     (concat lein-executer " clean && " lein-executer " deps && " lein-executer " compile && " lein-executer " uberjar"))))
 
 (defun lein-run-project ()
-	(interactive)
-	(let ((default-directory lein-project-directory))
-		(shell-command 
-			(concat lein-executer " run")
-		)
-	)
-)
+  (interactive)
+  (let ((default-directory lein-project-directory))
+    (shell-command 
+     (concat lein-executer " run"))))
+
+(defun lein-project-edit ()
+  (interactive)
+  (let ((file_path (path-concat lein-project-directory lein-project-file)))
+    (if (file-exists-p file_path)
+	(find-file file_path)
+      (error "Not opened project"))))
+
+(defun lein-view-project-directory ()
+  (interactive)
+  (let ((default-directory lein-project-directory))
+    (error "dired unimplemented")))
 
 (defun lein-interactive-repl-project ()
-	(interactive)
-	(let ((default-directory lein-project-directory))
-		(shell-command 
-			(concat lein-executer " repl")
-		)
-	)
-)
+  (interactive)
+  (error "unimplemented")
+  (let ((default-directory lein-project-directory))
+    (shell-command 
+     (concat lein-executer " repl"))))
 
 
-
-;clojure menu:
+					;clojure menu:
 (make-main-menu clojure 'tools)
 (make-menu clojure lein-interactive-repl-project "M-[ i")
 (make-menu clojure lein-run-project "M-[ r")
 (make-menu clojure lein-full-compile-project "M-[ f")
+(make-menu clojure lein-project-edit "M-[ p")
+(make-menu clojure lein-view-project-directory "M-[ v")
 (make-menu clojure lein-new-project "M-[ n")
-(make-menu clojure set-lein-project-directory "M-[ d")
+(make-menu clojure lein-set-project-directory "M-[ d")
 (make-menu clojure lein-execute-command "M-[ l")
 (make-menu clojure lein-help "M-[ h")
-;(make-menu clojure shell-command "M-[ s")
--- etc/startup.el
+++ etc/startup.el
@@ -1,109 +1,66 @@
-;usings:
+					;usings:
 (require 'cl)
 
 
-
-;common:
+					;common:
 (defmacro comment (&rest body)
   "Comment out one or more s-expressions."
   nil)
 
-  
-  
-;symbols:
+
+					;symbols:
 (defun replace-char (string replaced-char char-replacer) 
-	(concat 
-		(mapcar 
-			(lambda (x) 
-				(if 
-				    (= x replaced-char) 
-				    char-replacer 
-				  x 
-				)
-			) string 
-		)
-	)
-)
+  (concat 
+   (mapcar 
+    (lambda (x) 
+      (if (= x replaced-char) char-replacer x))
+    string)))
 
 (defun upper-first (s)
-	(if (> (length s) 0)
-		(concat (upcase (substring s 0 1)) (substring s 1))
-		nil
-	)
-)
+  (if (> (length s) 0)
+      (concat (upcase (substring s 0 1)) (substring s 1))
+    nil))
 
 (defun symbol-to-text (symbol) 
-	(upper-first 
-		(replace-char 
-			(symbol-name symbol) 
-			?- 
-			? 
-		)
-	)
-)
+  (upper-first 
+   (replace-char 
+    (symbol-name symbol) 
+    ?- 
+    ? )))
 
-(defmacro upper-first-symbol (symbol) `(make-symbol (upper-first (symbol-name ,symbol))))
+(defmacro upper-first-symbol (symbol)
+  `(make-symbol (upper-first (symbol-name ,symbol))))
 
 
+					;emacs-menus:
+(defmacro symbol-to-menu (symbol)
+  `(list* (symbol-to-text ,symbol) ,symbol))
 
-;emacs-menus:
-(defmacro symbol-to-menu (symbol) `(list* (symbol-to-text ,symbol) ,symbol))
-(defmacro make-main-menu (main_menu after_menu) 
-	`(define-key-after global-map [menu-bar ,main_menu] (cons ,(symbol-to-text main_menu) (make-sparse-keymap)) ,after_menu)
-);(make-main-menu main-Menu-Name 'afterThisMenu)
-(defmacro make-menu (main_menu command key_binding) 
-	`(progn 
-		(define-key global-map [menu-bar ,main_menu ,(upper-first-symbol command)] ',(symbol-to-menu command))
-		(global-set-key (kbd ,key_binding) ',command)
-	)
-);(make-menu mainMenuName command-for-execute-menu-item "M-[ h")
+(defmacro make-main-menu (main_menu after_menu)  ;(make-main-menu main-Menu-Name 'afterThisMenu) 
+  `(define-key-after global-map [menu-bar ,main_menu] (cons ,(symbol-to-text main_menu) (make-sparse-keymap)) ,after_menu))
 
+(defmacro make-menu (main_menu command key_binding)  ;(make-menu mainMenuName command-for-execute-menu-item "M-[ h") 
+  `(progn 
+     (define-key global-map [menu-bar ,main_menu ,(upper-first-symbol command)] ',(symbol-to-menu command))
+     (global-set-key (kbd ,key_binding) ',command)))
 
 
-;interactive:
+					;interactive:
 (defun read-string-not-whitespace (prompt &optional default_input parameter_name)
-	(let 
-		(
-			(default_input (if default_input default_input ""))
-			(readed (string-trim (read-string (concat prompt ": ") default_input)))
-			(parameter_name (if parameter_name parameter_name (downcase prompt)))
-		)
-		(if (equal "" readed) (error (concat "Error: Invalid " parameter_name ". Quit")) readed)
-	)
-)
+  (let ((default_input (if default_input default_input ""))
+	(readed (string-trim (read-string (concat prompt ": ") default_input)))
+	(parameter_name (if parameter_name parameter_name (downcase prompt))))
+    (if 
+	(equal "" readed)
+	(error (concat "Error: Invalid " parameter_name ". Quit")) 
+      readed)))
 
 
-
-;paths
+					;paths
 (defun path-concat (path new-part)
   (if (string-match ".*/$" path)
-    (concat path new-part)
+      (concat path new-part)
     (concat path "/" new-part)))
 
-;delete this:
 
-;(comment
-;(defun lein-help ()
-	  ;(interactive)
-	  ;(insert (shell-command-to-string "lein.bat help")))
-  
-;(define-key-after
-  ;global-map
-  ;[menu-bar mymenu]
-  ;(cons "Clojure" (make-sparse-keymap))
-  ;'tools )
-
-;(define-key
-  ;global-map
-  ;[menu-bar mymenu nl]
-  ;'("Lein help" . lein-help))
-
-;(global-set-key
-	 ;(kbd "M-[ h")
-	 ;'lein-help
-	 ;)
-	 
-;(defun lein-help ()
-;	  (interactive)
-;	  (insert (shell-command-to-string "lein.bat help")))
-;)
+					;files
